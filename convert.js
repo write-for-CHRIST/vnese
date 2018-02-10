cc139361939d6173ca39a0d7447c1e9d44e0f060
lanbao289 <lanbao289@gmail.com>
2018-02-10 18:38:33 +0700

    feat(table): add some table
    add table:  tcvn3, viqr, viscii, vps

--- convert.js
+++ convert.js
@@ -1,4 +1,9 @@
 const UNICODE_TABLE = require('./unicode')
+const VNI_TABLE = require('./vni')
+const TCVN3_TABLE = require('./tcvn3')
+const VPS_TABLE = require('./vps')
+const VISCII_TABLE = require('./viscii')
+const VIQR_TABLE = require('./viqr')
 
 module.exports = (str, fromTable, toTable) => {
   console.log('Not yet implemented!')
